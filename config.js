3788a7a126255c1e0e990f5480f6724662175210
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2025-02-15 23:23:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   amountToWrap: 0.0001, // Amount WPOL to wrap/convert to tPOL
-  autoRestartDelay: 1800 // Delay time in seconds for auto-restart
+  autoRestartDelay: 600 // Delay time in seconds for auto-restart
 };
